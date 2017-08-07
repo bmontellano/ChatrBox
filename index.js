299019606cb23472a10cd450c416fef37c506e7c
nicholas anggiansah <Mark@Hadys-MacBook-Pro.local>
2017-08-07 09:19:39 -0700

    socket.io

--- index.js
+++ index.js
@@ -7,20 +7,33 @@ const
   express = require('express'),
   app = express(),
   httpServer = require('http').Server(app),
-  socketServer = require('socket.io')(httpServer),
+  io = require('socket.io')(httpServer),
   PORT = process.env.PORT || 3000
 
+//mongodb
 mongoose.connect('mongodb://localhost/ChatrBox')
 
+//socket.io
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg)
+  })
+})
+
+
+
+
+//server
 httpServer.listen(PORT, function(err) {
   console.log(err || `Server running on port ${PORT}`)
 })
-  
+
+//view engine
 app.set('view engine', 'ejs')
 
+
+//route
 app.get('/', function(req,res) {
-  console.log('====================')
-  console.log('HI IM STARTING')
-  console.log('====================')
   res.render('index')
 })
