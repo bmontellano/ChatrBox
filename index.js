464079cd652fe00bc7b218a0164af1201173c54d
nicholas anggiansah <Mark@Hadys-MacBook-Pro.local>
2017-08-07 15:10:15 -0700

    removed token

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const
   cors = require('cors'),
   chatRoutes = require('./routes/chats.js'),
   httpServer = require('http').Server(app),
-  io = require('socket.io')(httpServer)
+  io = require('socket.io')(httpServer),
+  passport = require('passport')
 
 
 //mongodb
@@ -77,55 +78,5 @@ app.route('api/users/:id')
     })
   })
 
-//Log in route
-app.post('/api/authenticate', (req, res) => {
-  //find user by email from the request body
-  //when retreiving the user from the database, include the password
-  User.findOne({email: req.body.email}, '+password', (err, user) => {
-      //if no user is found / wrong password
-    if(!user || (user && !user.validPassword(req.body.password))) {
-      //return success: false and message
-      return res.json({success: false, message: "Incorrect email or password"})
-    }
-    //otherwise, use mongoose document's toObject() method to get a stripped down version of just the user's data
-    const userData = user.toObject()
-    //delete password from the object before creating the token
-    delete userData.password
-
-    //create token, embedding the user's info in the payload of the token
-    const token = jwt.sign(userData, process.env.SECRET)
-
-    //send the token back to the client in our response:
-    res.json({success: true, message: "Logged in successfully.", token})
-  })
-})
-
-// any routes declared after this middleware must include a valid token in order access:
-app.use(verifyToken)
-
 //all routes will be /api/beers/:id
 app.use('/api/chats', chatRoutes)
-
-
-//Middleware to restrict access.
-//user must include a token in their requests to process
-function verifyToken(req, res, next) {
-  //try to find token in headers:
-    const token = req.headers['token']
-    //if token exists
-    if(token) {
-      //verify tokens authenticity
-      jwt.verify(token, process.env.SECRET, (err, decoded) => {
-        //if error authenticating
-        if(err) return res.json({success: false, messages: "Token could not be verified."})
-        //Otherwise, get user info from decoded token
-        //and make it available from the req Object
-        req.user = decoded
-
-        next()
-      })
-    } else {
-      //if token not provided in the req headers
-      res.json({success: false, message: "No token provided. Access denied."})
-    }
-}
